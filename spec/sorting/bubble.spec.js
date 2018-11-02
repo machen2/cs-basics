87d5249467fc040104787f82fa4de2f7c5567bd7
Tori Machen <tmachen@pillartechnology.com>
2018-11-01 23:37:21 -0500

    Bubble Sort returns sorted array when given unsorted two element array

--- spec/sorting/bubble.spec.js
+++ spec/sorting/bubble.spec.js
@@ -2,6 +2,13 @@ const BubbleSort = require('../../src/sorting/bubble');
 
 describe('Bubble Sort', () => {
     it('returns an unmodified array when the array is already sorted', () => {
-        expect(BubbleSort([1,2,3,4])).toEqual([1,2,3,4]);
+        const alreadySortedArray = [1,2,3,4];
+        expect(BubbleSort(alreadySortedArray)).toEqual([1,2,3,4]);
     });
+
+    it('returns sorted array when given unsorted array with two elements', () => {
+        const unsortedArray = [2,1];
+        const expectedSortedArray = [1,2];
+        expect(BubbleSort(unsortedArray)).toEqual(expectedSortedArray);
+    })
 });
--- src/sorting/bubble.js
+++ src/sorting/bubble.js
@@ -1,4 +1,9 @@
 function BubbleSort(array) {
+    if(array[0] > array[1]) {
+        let temp = array[1];
+        array[1] = array[0];
+        array[0] = temp;
+    }
     return array;
 }
 
