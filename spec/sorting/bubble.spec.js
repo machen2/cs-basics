098f0788ee3be1fc7ecb565afd9b4cf8f326f6e2
Tori Machen <tmachen@pillartechnology.com>
2018-11-01 23:14:03 -0500

    initial setup with jasmine for testing

---
+++ spec/sorting/bubble.spec.js
@@ -0,0 +1,7 @@
+const BubbleSort = require('../../src/sorting/bubble');
+
+describe('Bubble Sort', () => {
+    it('returns true', () => {
+        expect(BubbleSort()).toEqual(true);
+    });
+});
---
+++ src/sorting/bubble.js
@@ -0,0 +1,5 @@
+function BubbleSort() {
+    return true;
+}
+
+module.exports = BubbleSort;
