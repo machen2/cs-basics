940a868dbd6d4f8645a64b316ee724b53676b7cc
Tori Machen <tmachen@pillartechnology.com>
2018-11-03 17:20:59 -0500

    confirm Mrge Sort works with one element

--- spec/sorting/merge.spec.js
+++ spec/sorting/merge.spec.js
@@ -1,6 +1,10 @@
 const MergeSort = require('../../src/sorting/merge');
 
 describe('Merge Sort', () => {
+    it('returns the array when there is only one element', () => {
+        expect(MergeSort([1])).toEqual([1]);
+    });
+
     it('returns the sorted array when given an already sorted array', () => {
         alreadySortedArray = [0,1];
         expect(MergeSort(alreadySortedArray)).toEqual([0,1]);
