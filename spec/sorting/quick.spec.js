ae8a624c23d3c48e2092b89be4f0e9056a0608d2
Tori Machen <tmachen@pillartechnology.com>
2018-11-03 23:23:41 -0500

    Quick Sort can sort two element array by swapping

--- spec/sorting/quick.spec.js
+++ spec/sorting/quick.spec.js
@@ -4,4 +4,10 @@ describe('Quick Sort', () => {
     it('returns the array when there is only one element', () => {
         expect(QuickSort([1])).toEqual([1]);
     });
+
+    it('returns sorted array when given unsorted two element array', () => {
+        let unsortedArray = [2,1];
+        const expectedArray = [1,2];
+        expect(QuickSort(unsortedArray)).toEqual(expectedArray);
+    });
 });
--- src/sorting/quick.js
+++ src/sorting/quick.js
@@ -1,4 +1,10 @@
 function quickSort(array) {
+    if(array.length > 1 && array[0] > array[1]) {
+        let temp = array[0];
+        array[0] = array[1];
+        array[1] = temp;
+    }
+
     return array;
 }
 
